7b8e793b51f7440c82742cade30fbb42a383e0a8
Ali Mirzakhalili <alimknu@gmail.com>
2023-11-28 23:15:28 -0500

    Registration management and event creation code added
    Registration management currently has nothing in it, the onClick was still created though so something can show up

--- Deliverable_2/app/src/main/java/com/example/deliverable_1/EventCreationActivity.java
+++ Deliverable_2/app/src/main/java/com/example/deliverable_1/EventCreationActivity.java
@@ -5,15 +5,37 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class EventCreationActivity extends AppCompatActivity {
 
-    private EditText eventTypeEditText, eventDetailsEditText;
-    private CheckBox under18CheckBox, eighteenToTwentyFiveCheckBox, twentySixToSixtyFiveCheckBox, over65CheckBox;
-    private CheckBox beginnerCheckBox, intermediateCheckBox, advancedCheckBox, expertCheckBox;
-    private CheckBox slowCheckBox, moderateCheckBox, fastCheckBox, extremeCheckBox;
-    private Button createEventButton;
+    Spinner eventSelectionSpinner;
+    TextView route, difficultyLevel, participantLimit;
 
+    Button createEventButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_creation);
+
+        // Initialize variables
+        eventSelectionSpinner = findViewById(R.id.eventSelectionSpinner);
+        route = findViewById(R.id.route);
+        difficultyLevel = findViewById(R.id.difficultyLevel);
+        participantLimit = findViewById(R.id.participantLimit);
+
+        createEventButton = findViewById(R.id.btnCreateEvent);
+
+        // Set click listener for the event creation button
+        createEventButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //create the event (NOT IMPLEMENTED)
+            }
+        });
+    }
 }
--- Deliverable_2/app/src/main/java/com/example/deliverable_1/RegistrationManagementActivity.java
+++ Deliverable_2/app/src/main/java/com/example/deliverable_1/RegistrationManagementActivity.java
@@ -1,6 +1,24 @@
 package com.example.deliverable_1;
 
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RegistrationManagementActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration_management);
+
+        // Initialize variables with findViewById(R.id.[idOfVariable]);
+        // variable = findViewById(R.id.[idOfVariable]);
+
+        // Set click listener for the submit button
+        /*submitRouteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                submitRoute();
+            }
+        });*/
+    }
 }
