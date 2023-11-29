dc5e2096611f3a3c782fdfec7b412706420ff4f7
Ali Mirzakhalili <alimknu@gmail.com>
2023-11-28 22:16:40 -0500

    Updated LoginActivity.java
    added support for club owner, added the given username and password for club owner: username = "gccadmin" password = "GCCRocks!"

--- Deliverable_2/app/src/main/java/com/example/deliverable_1/LoginActivity.java
+++ Deliverable_2/app/src/main/java/com/example/deliverable_1/LoginActivity.java
@@ -54,12 +54,10 @@ public class LoginActivity extends AppCompatActivity {
                 // redirect to admin page
                 Intent adminIntent = new Intent(LoginActivity.this, AdminDashboardActivity.class);
                 startActivity(adminIntent);
-            } else if (password.equals("clubowner")){
+            } else if (username.equals("gccadmin") && password.equals("GCCRocks!")){
                 String accountType = "Club Owner";
-                Intent homeIntent = new Intent(LoginActivity.this, HomePage.class);
-                homeIntent.putExtra("USERNAME", username);
-                homeIntent.putExtra("ACCOUNT_TYPE", accountType);
-                startActivity(homeIntent);
+                Intent clubOwnerIntent = new Intent(LoginActivity.this, ClubOwnerDashboardActivity.class);
+                startActivity(clubOwnerIntent);
             } else{
                 // for now assume user is participant
                 String accountType = "Participant";
