4f78bfb8e50d309331ba19cd45d712849fe62c63
Saja Elkurtehi <114425874+Saja-elkurtehi@users.noreply.github.com>
2023-11-28 21:49:23 -0500

    added routeplanning and clubownerdashboard
    added routeplanning and clubownerdashboard

--- Deliverable_2/app/src/main/java/com/example/deliverable_1/ClubOwnerDashboardActivity.java
+++ Deliverable_2/app/src/main/java/com/example/deliverable_1/ClubOwnerDashboardActivity.java
@@ -1,57 +1,57 @@
-package com.example.deliverable_1;
+package com.example.deliverable_3;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class ClubOwnerDashboardActivity {
-    public class AdminDashboardActivity extends AppCompatActivity {
+public class ClubOwnerDashboardActivity extends AppCompatActivity {
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_clubowner_dashboard);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clubowner_dashboard);
 
-            Button eventCreationButton = findViewById(R.id.btnEventCreation);
+        TextView welcomeText = findViewById(R.id.welcomeText);
+        welcomeText.setText("Welcome (Club Owner)!");
 
-            // / Set click listeners for the buttons
-            eventCreationButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    // Navigate to the EventManagementActivity
-                    startActivity(new Intent(com.example.deliverable_1.ClubOwnerDashboardActivity.this, EventCreationActivity.class));
-                }
-            });
+        Button eventCreationButton = findViewById(R.id.btnEventCreation);
+        eventCreationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to the EventCreationActivity
+                startActivity(new Intent(ClubOwnerDashboardActivity.this, EventCreationActivity.class));
+            }
+        });
 
-            Button routePlanningButton = findViewById(R.id.btnRoutePlanning);
-            routePlanningButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    // Navigate to the Account Management part of the admin panel
-                    startActivity(new Intent(com.example.deliverable_1.ClubOwnerDashboardActivity.this, RoutePlanningActivity.class));
-                }
-            });
+        Button routePlanningButton = findViewById(R.id.btnRoutePlanning);
+        routePlanningButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to the RoutePlanningActivity
+                startActivity(new Intent(ClubOwnerDashboardActivity.this, RoutePlanningActivity.class));
+            }
+        });
 
-            Button registrationManagementButton = findViewById(R.id.btnRegistrationManagement);
-            routePlanningButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    // Navigate to the Account Management part of the admin panel
-                    startActivity(new Intent(com.example.deliverable_1.ClubOwnerDashboardActivity.this, RegistrationManagementActivity.class));
-                }
-            });
+        Button registrationManagementButton = findViewById(R.id.btnRegistrationManagement);
+        registrationManagementButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to the RegistrationManagementActivity
+                startActivity(new Intent(ClubOwnerDashboardActivity.this, RegistrationManagementActivity.class));
+            }
+        });
 
-            Button resultsAndAwardsButton = findViewById(R.id.btnRoutePlanning);
-            routePlanningButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    // Navigate to the Account Management part of the admin panel
-                    startActivity(new Intent(com.example.deliverable_1.ClubOwnerDashboardActivity.this, ResultsAndAwardsActivity.class));
-                }
-            });
-        }
+        Button resultsAndAwardsButton = findViewById(R.id.btnResultsAndAwards);
+        resultsAndAwardsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to the ResultsAndAwardsActivity
+                startActivity(new Intent(ClubOwnerDashboardActivity.this, ResultsAndAwardsActivity.class));
+            }
+        });
     }
 }
--- Deliverable_2/app/src/main/java/com/example/deliverable_1/RoutePlanningActivity.java
+++ Deliverable_2/app/src/main/java/com/example/deliverable_1/RoutePlanningActivity.java
@@ -1,6 +1,62 @@
-package com.example.deliverable_1;
+package com.example.deliverable_3;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RoutePlanningActivity extends AppCompatActivity {
+
+    // Initialize views
+    private EditText destinationEditText, elevationEditText, landmarksEditText;
+    private Button submitRouteButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_route_planning);
+
+        // Initialize views
+        destinationEditText = findViewById(R.id.editTextDestination);
+        elevationEditText = findViewById(R.id.editTextElevation);
+        landmarksEditText = findViewById(R.id.editTextLandmarks);
+        submitRouteButton = findViewById(R.id.buttonPlanRoute);
+
+        // Set click listener for the submit button
+        submitRouteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                submitRoute();
+            }
+        });
+    }
+
+    private void submitRoute() {
+        // Get values from EditText fields
+        String destination = destinationEditText.getText().toString().trim();
+        String elevation = elevationEditText.getText().toString().trim();
+        String landmarks = landmarksEditText.getText().toString().trim();
+
+        // Check if fields are not empty
+        if (!destination.isEmpty() && !elevation.isEmpty() && !landmarks.isEmpty()) {
+            // Process the route data as needed (e.g., display in a Toast)
+            String routeDetails = "Destination: " + destination +
+                    "\nElevation: " + elevation +
+                    "\nLandmarks: " + landmarks;
+
+            // Display a success message or perform any other actions
+            Toast.makeText(this, "Route submitted successfully:\n" + routeDetails, Toast.LENGTH_SHORT).show();
+
+            // Clear EditText fields after submission
+            destinationEditText.setText("");
+            elevationEditText.setText("");
+            landmarksEditText.setText("");
+        } else {
+            // Display an error message if any field is empty
+            Toast.makeText(this, "All fields must be filled", Toast.LENGTH_SHORT).show();
+        }
+    }
 }
